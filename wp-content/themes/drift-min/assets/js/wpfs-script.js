7e6a636d77f4dc3d3362a838d45e45c8a2668377
Pea <pea@misfist.com>
2022-05-06 01:00:30 -0400

    Remove console.log

--- wp-content/themes/drift-min/assets/js/wpfs-script.js
+++ wp-content/themes/drift-min/assets/js/wpfs-script.js
@@ -13,28 +13,9 @@
     const fieldNodes = $( '.wpfs-form-check-group .wpfs-form-check' );
 
     if( fieldNodes ) {
-        console.log( formType );
         if( 'inline_subscription' === formType ) {
             formatSubscriptions();
         }
-        // fieldNodes.each( function( index, value ) {
-        //     let $input = $( this ).find( 'input' );
-        //     let $label = $( this ).find( '.wpfs-form-check-label' );
-        //     let $amount =  $input.data( 'wpfs-plan-amount' );
-        //     let $currency = $input.data( 'wpfs-currency' );
-        //     let $planName = $input.data( 'wpfs-plan-name' );
-
-        //     let amount = new Intl.NumberFormat( 'en-US', { 
-        //         style: 'currency', 
-        //         currency: $currency,
-        //         minimumFractionDigits: 0,
-        //         maximumFractionDigits: 2
-        //     } ).format( $amount );
-
-        //     let $description = '<span class="constrained">' + $planName + '</span>';
-        //     let $price = '<strong>' + amount + '</strong>';
-        //     $label.html( $description + $price );
-        // } );
 
         if( 'inline_payment' === formType ) {
             formatPayments();
